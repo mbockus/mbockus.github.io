546b8b942310d4b6e04b7fb3d9ec16592c5afda9
mbockus <mike@bockosoft.com>
2015-10-20 01:39:03 -0500

    Added the demo for blast and plumin. Failed to get my emoji font going.

--- talks/important/js/blastdemo.js
+++ talks/important/js/blastdemo.js
@@ -1,3 +1,241 @@
-/**
- * Created by MBockus on 10/9/2015.
- */
+/************
+ Demos
+ ************/
+
+var $description = $("#description");
+
+/* Blast undo button. */
+function showUndoButton () {
+    $("#btnUndo").fadeIn(500)
+}
+
+$("#btnUndo").on("click", function () {
+    $description.blast(false);
+    $(this).fadeOut(500);
+});
+
+/* Delimiter buttons. */
+$("#buttons input[type='button']").on("click", function () {
+    var $this = $(this),
+        delimiter,
+        $generatedElements;
+
+    if ($this.attr("id") === "btnUndo") {
+        return true;
+    } else {
+        showUndoButton();
+    }
+
+    $this
+        .addClass("btnClicked")
+        .siblings()
+        .removeClass("btnClicked");
+
+    if ($this.attr("name") === "search") {
+        delimiter = $.trim($("#buttons #textBtnSearch").val()) || "blast";
+
+        $generatedElements = $description.blast( { search: delimiter } );
+    } else {
+        delimiter = $this.attr("name");
+
+        $generatedElements = $description.blast( { delimiter: delimiter } );
+    }
+
+    /* Fade in a background-color for search results. */
+    if ($this.attr("name") === "search") {
+        $generatedElements
+            .css({
+                backgroundColor: "rgba(223, 231, 236, 0)",
+                transition: "color 400ms"
+            })
+            .velocity(
+            {
+                backgroundColorAlpha: 1
+            }, {
+                duration: 400
+            });
+    }
+
+    /* Alternate colors between generated elements. */
+    $generatedElements
+        .filter(":even")
+        .css("color", "#0c5d99")
+        .end()
+        .filter(":odd")
+        .css("color", "#dd1f7b");
+});
+
+/* Event bindings for the sample button. */
+$description.find("#btnSample").on("click", function () {
+    alert("This click event is bound directly.");
+});
+
+$("body").on("click", "#btnSample", function () {
+    alert("This click event is bound to a parent element.");
+});
+
+/* Demos. */
+$("#description > li a").on("click", function () {
+    showUndoButton();
+
+    switch ($(this).attr("id")) {
+        case "btnTypographicAnimation":
+            $("li").eq(0).blast()
+                .velocity("fadeOut", { duration: 350, stagger: 30, drag: true, backwards: true, display: null, complete: function() {
+                    $("li").eq(0).find(".blast")
+                        .velocity("fadeIn", { delay: 500, stagger: 30, drag: true })
+                        .velocity("transition.slideDownIn", { delay: 1000, stagger: 20, drag: true, display: "inline-block", complete: function() {
+                            $("li").eq(0).blast(false);
+                            $("#btnUndo").trigger("click");
+                        }
+                        });
+                }
+                })
+            break;
+
+        case "btnStylingDemo":
+            $description
+                .blast({ delimiter: "word" })
+                .each(function (i) {
+                    $(this).css({ opacity: i / (3.5 * 100) });
+                });
+
+            alert("The styling demo separates text using the word delimiter then iteratively increases the opacity of each word. Because no special CSS3 gradient properties are being used to achieve this fading effect, it works across all browsers -- all the way back to IE6.");
+            break;
+
+        case "btnJuxtapositionDemo":
+            var phraseLetters = (prompt("Enter your first name:").toLowerCase().replace(/\s/g, "") || "blast").split(""),
+                letterMatchCount = 0;
+
+            $description
+                .blast({ delimiter: "character" })
+                .each(function () {
+                    var element = this;
+
+                    $.each(phraseLetters, function (i, letter) {
+                        if (element.innerHTML === letter) {
+                            phraseLetters[i] = false;
+                            letterMatchCount++;
+
+                            $(element)
+                                .attr("juxtaposition-demo-index", i)
+                                .css({
+                                    position: "fixed",
+                                    color: "rgb(0, 125, 255)"
+                                })
+                                .velocity({ fontSize: "45px" }, { duration: 350 });
+
+                            return false;
+                        }
+
+                        if (letterMatchCount === phraseLetters.length) {
+                            return false;
+                        }
+                    });
+                });
+
+            if (letterMatchCount !== phraseLetters.length) {
+                alert("Sorry, couldn't match all the letters in your name.");
+            } else {
+                var fontSizeOriginal = $description.css("font-size"),
+                    documentWidth = document.body.offsetWidth,
+                    documentHeight = document.body.clientHeight;
+
+                $.each(phraseLetters, function(i, letter) {
+                    $description.find("[juxtaposition-demo-index=" + i + "]").each(function () {
+                        var $this = $(this),
+                            offset = $this.offset();
+
+                        $this
+                            .css("display", "inline-block")
+                            .delay(i * 350)
+                            .velocity(
+                            {
+                                translateX: documentWidth/2 - offset.left - (25 * phraseLetters.length) + (i * 45),
+                                translateY: 300 - offset.top
+                            }, {
+                                duration: 750
+                            }
+                        )
+                            .delay(2250)
+                            .velocity(
+                            {
+                                translateX: 0,
+                                translateY: 0
+                            }, {
+                                complete: function() {
+                                    $this
+                                        .css({
+                                            display: "inline",
+                                            position: "static",
+                                            color: "inherit"
+                                        })
+                                        .velocity({ fontSize: fontSizeOriginal }, {
+                                            duration: 1000,
+                                            complete: function() {
+                                                if (i === phraseLetters.length - 1) {
+                                                    $("#btnUndo").trigger("click");
+                                                }
+                                            }
+                                        });
+                                }
+                            }
+                        );
+                    });
+                });
+            }
+            break;
+
+        case "btnSearchDemo":
+            $description
+                .blast({ delimiter: "word" })
+                .each(function() {
+                    if (Math.random() < 0.075) {
+                        var $this = $(this);
+
+                        $this
+                            .css({
+                                display: "inline-block",
+                                backgroundColor: "rgba(255, 255, 0, 0.85)"
+                            })
+                            .velocity({ scaleX: 2, scaleY: 2, translateZ: 0 }, { duration: 1500 })
+                            .velocity("reverse", {
+                                complete: function() {
+                                    $this
+                                        .css({
+                                            display: "inline",
+                                            backgroundColor: "transparent"
+                                        })
+                                        .blast(false);
+
+                                    $("#btnUndo").trigger("click");
+                                }
+                            });
+                    }
+                });
+            break;
+
+        case "btnAnalysisDemo":
+            var longSentenceCount = 0;
+
+            showUndoButton();
+
+            alert("The analysis demo color-fills each sentence to provide a visual overview of your page's text density.");
+
+            $description
+                .blast({ delimiter: "sentence" })
+                .each(function(i) {
+                    var $this = $(this);
+                    fillColor = i % 2 ? "#262626" : "#dadada";
+
+                    $this.css({ color: fillColor, backgroundColor: fillColor });
+
+                    if ($this.html().length > 150) {
+                        longSentenceCount++;
+                    }
+                });
+
+            alert("Results: " + longSentenceCount + " sentences are over 150 characters. Consider trimming them for increased legibility.\r\n\r\nClick the Undo button (below) to remove the color-fills.");
+            break;
+    }
+});
--- talks/important/js/plumindemo.js
+++ talks/important/js/plumindemo.js
@@ -1,3 +1,128 @@
-/**
- * Created by MBockus on 10/9/2015.
- */
+(function(p) {
+    'use strict';
+
+// Setup paper.js by providing a canvas element
+    p.setup( document.getElementById('canvas') );
+
+    var font = new p.Font({
+            familyName: 'Demo',
+            ascender: 800,
+            descender: -200
+        }),
+
+    // glyphs
+        A = new p.Glyph({
+            name: 'A',
+            unicode: 'A',
+            advanceWidth: 512
+        }),
+        B = new p.Glyph({
+            name: 'B',
+            unicode: 'B',
+            advanceWidth: 512
+        }),
+        C = new p.Glyph({
+            name: 'C',
+            unicode: 'C',
+            advanceWidth: 512
+        }),
+        a = new p.Glyph({
+            name: 'a',
+            unicode: 'a',
+            advanceWidth: 496
+        }),
+        i = new p.Glyph({
+            name: 'i',
+            unicode: 'i',
+            advanceWidth: 268
+        }),
+        o = new p.Glyph({
+            name: 'o',
+            unicode: 'o',
+            advanceWidth: 536
+        }),
+        shape,
+        counter;
+
+// A contours
+    shape = new p.Path.RegularPolygon({
+        center: [256, 136],
+        sides: 3,
+        radius: 256
+    });
+    counter = new p.Path.RegularPolygon({
+        center: [256, 61],
+        sides: 3,
+        radius: 256
+    });
+    shape.rotate(180, [256, 136]);
+    counter.rotate(180, [256, 61]);
+    A.addContour(shape.subtract(counter));
+    A.scale(1,2, [256, 0]);
+
+// B contours
+    shape = new p.Path.Rectangle({
+        point: [0, 0],
+        size: [450, 450]
+    });
+    counter = new p.Path.Rectangle({
+        point: [40, 40],
+        size: [370, 370]
+    });
+    B.addContour(shape.subtract(counter));
+    shape = new p.Path.Rectangle({
+        point: [0, 410],
+        size: [340, 340]
+    });
+    counter = new p.Path.Rectangle({
+        point: [40, 450],
+        size: [260, 260]
+    });
+    B.addContour(shape.subtract(counter));
+
+// C contours
+    shape = new p.Path.Ellipse({
+        point: [0, 0],
+        size: [520, 800]
+    });
+    counter = new p.Path.Ellipse({
+        point: [40, 40],
+        size: [440, 720]
+    });
+    shape = shape.subtract(counter);
+    counter = new p.Path.Rectangle({
+        point: [430, 180],
+        size: [100, 440]
+    });
+    C.addContour(shape.subtract(counter));
+
+    // a contour
+    shape = new p.Path.RegularPolygon({
+        center: [268, 124],
+        sides: 3,
+        radius: 248
+    });
+    shape.rotate(180, [268, 124]);
+    shape.scale(1, 1.4, [268, 0]);
+    a.addContour(shape);
+
+// i contour
+    shape = new p.Path.Rectangle({
+        point: [60, 0],
+        size: [140, 500]
+    });
+    i.addContour(shape);
+
+// o contour
+    shape = new p.Path.Ellipse({
+        point: [50, 0],
+        size: [436, 510]
+    });
+    o.addContour(shape);
+
+    font.addGlyphs([ A, B, C, a, i, o ]);
+    font.updateOTCommands()
+        .addToFonts();
+
+})(plumin.paper);
+				
